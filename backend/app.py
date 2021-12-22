34b10a36155ae4dda08865f1997941aaf64aaec8
naamaah <naamaah@post.bgu.ac.il>
2021-12-22 18:40:23 +0200

    assignment9

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,8 @@
-"""
-Assigment7
-@app.route('/home')
-@app.route('/')
-def homeFunc():
-    return 'welcome to homePage'
-
-@app.route('/about', methods=['GET', 'POST'])
-def aboutFun ():
-    return redirect('/') #redirect to homePage
-
-@app.route('/catalog')
-def catalog():
-    return redirect(url_for(homeFunc))
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-"""
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session, url_for
 app=Flask(__name__)
+app.secret_key = '123'
+
+
 
 @app.route('/home')
 @app.route('/')
@@ -34,7 +18,46 @@ def assignmentFun ():
                            profile={'name': name, 'lastName': lastName},  # dict
                            university=uni,
                            hobbies=('Gym', 'Kangoo-jump', 'Netflix', 'Movies'),  # tuple
-                            currentTV = ['friends', 'X-factor'])   #tuple
+                            currentTV = ['friends', 'X-factor'])
+
+
+
+#assignmnet9 functions:
+users = {'user1': {'name': 'Yossi', 'email': 'yossi@gmail.com'},
+         'user2': {'name': 'Naama', 'email': 'naama10@gmail.com'},
+         'user3': {'name': 'Ron', 'email': 'ron1995@gmail.com'},
+         'user4': {'name': 'Evia', 'email': 'evia98@gmail.com'},
+         'user5': {'name': 'Yotam', 'email': 'yotam29@gmail.com'} }
+
+def checkInput (input):
+    for user in users.values():
+        if (user['email'] == input or user['name'] == input):
+            return (user['email'],  user['name'])
+    return False #the input didnt exits in the list or it is empty
+
+@app.route('/assignment9' , methods=['GET','POST'])
+def assignment9Func ():
+    if request.method == 'GET':
+        if 'searchInput' in request.args:
+            print (request.args['searchInput'])
+            if (checkInput(str(request.args['searchInput'])) != False):
+                email = checkInput(request.args['searchInput'])[0]
+                name = checkInput(request.args['searchInput'])[1]
+                return render_template('assignment9.html', name=name, email=email)
+        return render_template('assignment9.html', users=users) #else return all user dict
+    if request.method == 'POST':
+        nickname=request.form['nickName'] #key name from login page
+        session['username'] = nickname
+        return render_template("assignment9.html", nickname=nickname, users=users)
+    return render_template('assignment9.html', users=users)
+
+
+@app.route('/logout', methods=['GET','POST'])
+def logOutFunc ():
+    session['username'] = ''
+    return redirect (url_for('assignment9Func'))
+
 
 if __name__ == "__main__":
+    app.secret_key = '123'
     app.run(debug=True)
